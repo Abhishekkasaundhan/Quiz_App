fea7f5a870561031eb6645afa78254ea4f0a0cc4
Abhishek Kasaundhan <abhishek2001ckt@gmail.com>
2022-09-18 14:14:05 +0530

    files

---
+++ app/src/main/java/net/practicalcoding/quizapp/QuestionAnswer.java
@@ -0,0 +1,30 @@
+package net.practicalcoding.quizapp;
+public class QuestionAnswer {
+
+    public static String question[] =
+            {"Which one is not programming language",
+             "Synonyms of ALERT",
+             "The study of ancient societies",
+             "Antonyms of MORTAL", "Which one is a valid declaration of a boolean?"
+
+    };
+
+    public static String choices[][] =
+            {
+                    {"Java","Kotlin","Notepad","Python"},
+            {"Energetic","Observant","Intelligent","Watchful"},
+
+            {"Anthropology","Archaeology","History","Ethonology"},
+            {"Divine","Immortal","Spiritual","Eternal"}
+                    ,{"boolean b1 = 0;","boolean b2 = 'false';" , "boolean b3 = false;" , "boolean b4 = Boolean.false();"}
+
+    };
+
+    public static String correctAnswer[] =
+            {
+            "Notepad",
+            "Watchful",
+            "Archaeology",
+            "Immortal", "boolean b3 = false;"
+    };
+}
---
+++ app/src/main/java/net/practicalcoding/quizapp/SplashScreen.java
@@ -0,0 +1,28 @@
+package net.practicalcoding.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        Intent i = new Intent(SplashScreen.this , MainActivity.class);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        },3000);
+    }
+}
